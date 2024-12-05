cc80ea4088377ece33847256681a066098c22c5a
Itay Mirkin <mirkinitay@gmail.com>
2024-12-05 20:37:55 +0200

    HW 2 Start commit
    *Working with struct
    * Parsing Done
    TODO:
    ** Need to add function to preform the jobs
    ** Add jobs fucntions in the parse cmds func
    ** Add wait func for dispatcher
    
    HOW TO RUN:
    
    *./hw2 command.txt num num enable(1 or 0)

--- hw1shell.c
+++ hw1shell.c
@@ -13,43 +13,117 @@ pthread_t* worker_trds;
 pthread_mutex_t work_queue_lock;
 pthread_cond_t work_available;
 
+typedef enum {
+    DIS_WAIT,
+    CMD_MSLEEP,
+    CMD_INCREMENT,
+    CMD_DECREMENT,
+    CMD_REPEAT
+} CmdType;
+
+typedef struct
+{
+    CmdType type;
+    int value; 
+} cmd_s;
+
+typedef struct 
+{
+    cmd_s * cmds;
+    int num_of_cmds;
+    int is_dispatcher; //1 - dispatcher, 1 - worker
+    char * line;        //logging
+} cmd_line_s;
 //* Globals *//
 
 int num_threads;          
 int num_counters;
 int log_enabled;
+void *dispatcher(void *arg) {
+    // Placeholder worker thread logic
+    printf("Worker thread running\n");
+    return NULL;
+}
+void dispatcher_wait() {};
 
+void *worker(void *arg) {
+    // Placeholder worker thread logic
+    printf("Worker thread running\n");
+    return NULL;
+}
 void *trd_func(void *arg) {
     // Placeholder worker thread logic
     printf("Worker thread running\n");
     return NULL;
 }
-void proccess_cmd(char *cmd) {
-    printf("Worker thread running\n");
-    
 
+cmd_s parse_cmds(char *cmd_str) {
+    
+    cmd_s cmd;
+    
+    //char command[MAX_LINE_LENGTH];
+    int value;
+    
+    if (sscanf(cmd_str, " msleep %d", &value) == 1) {
+        cmd.type = CMD_MSLEEP;
+        cmd.value = value;
+    }
+    else if (sscanf(cmd_str, " increment %d", &value) == 1) {
+        cmd.type = CMD_INCREMENT;
+        cmd.value = value;
+    }
+    else if (sscanf(cmd_str, " decrement %d", &value) == 1) {
+        cmd.type = CMD_DECREMENT;
+        cmd.value = value;
+    }
+    else if (sscanf(cmd_str, " repeat %d", &value) == 1) {
+        cmd.type = CMD_REPEAT;
+        cmd.value = value;
+    }
+    
+    return cmd;
 }
 
 
 
 //tokenize cmd line by semi columns 
-void dispatcher_command(char *line) {
-    char *token;
-    token = strtok(line, ";");
-    while (token != NULL) {
-        while (*token == ' ') token++; //rmv spaces 
-        char *end = token + strlen(token) - 1;
-        while (end > token && (*end == ' ' || *end == '\n')) {
-            *end = '\0';
-            end--;
+cmd_line_s*  parse_line(char *line) {
+    cmd_line_s* cmd_line = (cmd_line_s *)malloc(sizeof(cmd_line_s));
+    cmd_line->cmds = (cmd_s*)malloc(MAX_LINE_LENGTH);
+    cmd_line->num_of_cmds = 0;
+    cmd_line->line = strdup(line);
+
+     if (strncmp(line, "dispatcher wait", 15) == 0) {
+       cmd_line->is_dispatcher = 1;
+       dispatcher_wait();
+       cmd_s dis_cmd;
+       if (strstr(line, "wait") != NULL) {
+            dis_cmd.type = DIS_WAIT;  
+            dis_cmd.value = 0; //just to assign something
+        } else {
+            dis_cmd = parse_cmds(line + 10); //10 = len dispatcehr  
         }
-
-        //proccess cmd line
-        proccess_cmd(token);
-
-        // Get the next token
-        token = strtok(NULL, ";"); //continue tokenize
+    cmd_line->cmds[0]= dis_cmd;
+    cmd_line->num_of_cmds =1;
+    return cmd_line;
     }
+
+    cmd_line->is_dispatcher =0;
+    char* token;
+    char* line_copy = strdup(line);
+    
+    // skip woeker word
+    token = strtok(line_copy, " ");
+    token = strtok(NULL, ";");
+    
+    while (token != NULL) {
+        cmd_line->cmds[cmd_line->num_of_cmds] = parse_cmds(token);
+        cmd_line->num_of_cmds++;
+        token = strtok(NULL, ";");
+    }
+    
+    free(line_copy);
+    return cmd_line;
 }
 
 
@@ -61,19 +135,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     //init main args
-    num_threads = atoi(argv[1]);
-    num_counters = atoi(argv[2]);
-    log_enabled = atoi(argv[3]);
-     
+    num_threads = atoi(argv[2]);
+    num_counters = atoi(argv[3]);
+    log_enabled = atoi(argv[4]);
+    printf("num of counters - %d \nnum of threads - %d\nlog enabled - %d\n", num_counters, num_threads, log_enabled); 
+
     //Create countes files and init them 
     char counter_filename[50];
     for (int i = 0; i < num_counters; i++)
     {
         sprintf(counter_filename, "count%02d.txt", i);
-        FILE *f = fopen(counter_filename, "W");
+        printf("file name - %s\n", counter_filename);
+        FILE *f = fopen(counter_filename, "w");
         if (f == NULL)
         {
-            printf("FILE CREATION ERROR");
+            printf("FILE CREATION ERROR HEREs");
             return 1;
         }
         fprintf(f, "0\n");
@@ -105,7 +181,8 @@ int main(int argc, char *argv[]) {
             //loaded a line
             line[strcspn(line, "\n")] = '\0';
             //tokenize line and excute
-            dispatcher_command(line);
+            printf("LINE as line : %s\n", line);
+            parse_line(line);
     }
     return 0;
 }
